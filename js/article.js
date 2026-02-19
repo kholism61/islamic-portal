6bf014bcc0d5c63121570bf6fae8f6c6c31604c0
kholism61 <nurcholism51@gmail.com>
2026-02-19 03:40:04 +0200

    Update article.js

--- js/article.js
+++ js/article.js
@@ -2117,36 +2117,3 @@ updateReadingStreak();
     </a>
   `).join("");
 })();
-
-
-// =====================
-// LANGUAGE DROPDOWN
-// =====================
-const langBtn = document.getElementById("langBtn");
-const langDropdown = document.getElementById("langDropdown");
-
-if (langBtn && langDropdown) {
-  langBtn.onclick = (e) => {
-    e.stopPropagation();
-    langDropdown.classList.toggle("active");
-  };
-
-  document.addEventListener("click", (e) => {
-    if (!langDropdown.contains(e.target)) {
-      langDropdown.classList.remove("active");
-    }
-  });
-}
-
-window.setLang = function(lang) {
-
-  localStorage.setItem("siteLang", lang);
-
-  if (lang === "id") {
-    document.cookie = "googtrans=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-  } else {
-    document.cookie = "googtrans=/id/" + lang + ";path=/";
-  }
-
-  location.reload();
-};

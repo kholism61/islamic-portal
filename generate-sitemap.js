1f19784232f7cb067244caf5bcba08cbce54bfb3
kholism61 <nurcholism51@gmail.com>
2026-02-19 04:20:30 +0200

    Update generate-sitemap.js

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -3,16 +3,27 @@ import fs from "fs";
 
 const baseUrl = "https://islamic-portal.vercel.app";
 
-let urls = `
-  <url>
-    <loc>${baseUrl}/</loc>
-  </url>
+let urls = "";
+
+// Homepage
+urls += `
+<url>
+  <loc>${baseUrl}</loc>
+  <changefreq>weekly</changefreq>
+  <priority>1.0</priority>
+</url>
 `;
 
+// Loop semua artikel
 Object.keys(articlesData).forEach((id) => {
+  const article = articlesData[id];
+
   urls += `
   <url>
     <loc>${baseUrl}/article.html?id=${id}</loc>
+    <lastmod>${article.createdAt}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
   </url>
   `;
 });

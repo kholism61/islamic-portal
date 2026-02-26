514487090c18b59bd48340e996872ad0afa99a66
kholism61 <nurcholism51@gmail.com>
2026-02-26 03:19:03 +0200

    Delete js/sw-register.js

--- js/sw-register.js
+++
@@ -1,7 +0,0 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js")
-      .then(reg => reg.update())
-      .catch(err => console.log("SW error:", err));
-  });
-}
